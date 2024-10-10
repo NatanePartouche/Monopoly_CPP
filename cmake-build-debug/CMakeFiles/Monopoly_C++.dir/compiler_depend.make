0d04abd114b377ba1298b9a886779e58d02c81e3
NatanePartouche <120121010+NatanePartouche@users.noreply.github.com>
2024-10-10 13:50:23 +0400

    Delete cmake-build-debug directory

--- cmake-build-debug/CMakeFiles/Monopoly_C++.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for Monopoly_C++.
-# This may be replaced when dependencies are built.
--- cmake-build-debug/CMakeFiles/Monopoly_Tests.dir/Tests/SpecialSquare_Tests.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Monopoly_Tests.dir/Tests/SpecialSquare_Tests.cpp.o: \
- /home/ubuntu/CLionProjects/Monopoly_CPP/Tests/SpecialSquare_Tests.cpp \
- /usr/include/stdc-predef.h
